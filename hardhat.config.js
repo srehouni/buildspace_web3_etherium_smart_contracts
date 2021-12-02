f6459984cb21beb0bac20433713730f741994157
Said Rehouni <said_rehouni@hotmail.com>
2021-12-03 00:54:12 +0100

    Added -env

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,4 +18,15 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: process.env.STAGING_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    mainnet: {
+      chainId: 1,
+      url: process.env.PROD_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
